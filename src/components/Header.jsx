a74c0bc2b3cf6a955ad1d5cd1572932fe49986ea
SALOHIDDIN <tursunovsalohiddin09@gmail.com>
2023-04-09 18:56:42 +0500

    rokterik

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <header className="header">
+            <img src="https://yt3.ggpht.com/Q8bNoC4WBo85WTwodaby171MkeLRaI7vmT7_QXM-J7qq3HZWFkHvLTykyrwPVkQA7eDCnZTN2A=s600-c-k-c0x00ffffff-no-rj-rp-mo"></img>
+        </header>
+    )
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Profile = () => {
+    return(
+        <div className='content'>
+        <div>
+        <img src='https://imgs.search.brave.com/PdkI_aJ5jNea1AiIas6uy7LMm_H-szZFY9fbTVjNaS4/rs:fit:1200:1080:1/g:ce/aHR0cHM6Ly9tYXJr/ZXRpbmdhY2Nlc3Nw/YXNzLmNvbS93cC1j/b250ZW50L3VwbG9h/ZHMvMjAxNS8xMC9Q/b2RjYXN0LVdlYnNp/dGUtRGVzaWduLUJh/Y2tncm91bmQtSW1h/Z2UuanBn'></img>
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          My posts
+          <div>
+            New posts
+          </div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+         
+      </div>
+    )
+
+}
+export default Profile;
